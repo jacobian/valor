adee90fa143dcbff7f4a60fdac71ec4e1c35454b
Jacob Kaplan-Moss <jacob@jacobian.org>
2014-07-12 16:53:27 -0500

    Added a model "identity" function, and used it for __repr__.

--- tests/test_model.py
+++ tests/test_model.py
@@ -19,3 +19,18 @@ def test_model_setattr(schema):
     a = App(name='my-app')
     a.name = 'changed'
     assert a.name == 'changed'
+
+def test_model_identity_not_id(schema):
+    App = model.model_factory('App', schema, schema['definitions']['app'])
+    a = App(id='1234', name='my-app')
+    assert a.identity() == 'my-app'
+
+def test_model_identity_only_id(schema):
+    AppSetup = model.model_factory('AppSetup', schema, schema['definitions']['app-setup'])
+    a = AppSetup(id='1234')
+    assert a.identity() == '1234'
+
+def test_model_repr(schema):
+    App = model.model_factory('App', schema, schema['definitions']['app'])
+    a = App(id='1234', name='my-app')
+    assert repr(a) == '<App: my-app>'
--- valor/model.py
+++ valor/model.py
@@ -1,3 +1,5 @@
+from .utils import is_ref
+
 def model_factory(name, schema, model_schema):
     return type(name, (Model,), {'schema': schema, 'model': model_schema})
 
@@ -13,3 +15,57 @@ class Model(object):
 
     def __setattr__(self, attr, value):
         self._dict[attr] = value
+
+    def __repr__(self):
+        identity = self.identity()
+        if identity:
+            return '<%s: %s>' % (self.__class__.__name__, identity)
+        else:
+            return object.__repr__(self)
+
+    def identity(self):
+        """
+        Return the "identity" of this object -- e.g. its ID, name, etc.
+
+        This is used in URIs identifying the object (e.g. /app/the-app-name),
+        and also for the model's __repr__ and such.
+
+        Most models will have both an ID (UUID) identity *and* something more
+        human-readable like a name, email, etc. If possible, try to use the
+        thing-that's-not-an-ID, since the human-readbale names read better.
+        """
+        model_defs = self.__class__.model['definitions']
+
+        # Look for multiple identity options
+        if 'identity' in model_defs:
+            if 'anyOf' in model_defs['identity']:
+                idschemas = model_defs['identity']['anyOf']
+            else:
+                idschemas = [model_defs['identity']]
+        else:
+            idschemas = []
+
+        # Poke through the identities, trying to find one we like. Identities
+        # have to be refs -- because they point to other fields, and aren't
+        # fields in their own right -- so we can just assume that the field name
+        # is the last part of the ref. We're looking for something that's not
+        # `id`, so build up a list of field names, shuffling anything that looks
+        # like an ID to the back. We'll use the first item in this list once we
+        # get through the loop.
+        candidates = []
+        for candidate in idschemas:
+            # Skip anything that's not a ref. Hm. Is this OK?
+            if not is_ref(candidate):
+                continue
+
+            field_name = candidate['$ref'].split('/')[-1]
+            if field_name == 'id':
+                candidates.append(field_name)
+            else:
+                candidates.insert(0, field_name)
+
+        # If we don't have any candidates now, try and 'id' field as a
+        # last-ditch effort.
+        cfield = candidates[0] if candidates else 'id'
+        return getattr(self, cfield, None)
+
